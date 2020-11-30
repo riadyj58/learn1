dbff63e12d1d37a70ee5b111d3f609e0d26b1509
JAMES RIADY <U069553@bca>
2020-11-30 09:45:25 +0700

    first commit

---
+++ src/main/java/com/learn/learn1/Controller.java
@@ -0,0 +1,14 @@
+package com.learn.learn1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    @GetMapping("/")
+    public String index()
+    {
+        return "String";
+    }
+}
